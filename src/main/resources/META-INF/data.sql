03a4217a6bda1d430a0ffccb290d08875c70e7d0
Mats Kruskopf Eriksson <mats.kruskopf.eriksson@iths.se>
2024-02-22 11:40:40 +0100

    add data.sql and schema.sql

---
+++ src/main/resources/META-INF/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO movie (title, releaseYear, director, rating, genre)
+VALUES
+    ('The Shawshank Redemption', 1994, 'Frank Darabont', 9.3, 'Drama'),
+    ('The Godfather', 1972, 'Francis Ford Coppola', 9.2, 'Crime'),
+    ('The Dark Knight', 2008, 'Christopher Nolan', 9.0, 'Action'),
+    ('Pulp Fiction', 1994, 'Quentin Tarantino', 8.9, 'Crime'),
+    ('Forrest Gump', 1994, 'Robert Zemeckis', 8.8, 'Drama'),
+    ('The Matrix', 1999, 'Lana and Lilly Wachowski', 8.7, 'Action'),
+    ('Inception', 2010, 'Christopher Nolan', 8.8, 'Sci-Fi'),
+    ('Titanic', 1997, 'James Cameron', 7.8, 'Romance'),
+    ('The Social Network', 2010, 'David Fincher', 7.7, 'Biography'),
+    ('La La Land', 2016, 'Damien Chazelle', 8.0, 'Musical');
---
+++ src/main/resources/META-INF/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE movies;
+
+CREATE TABLE movie (
+                       Id int NOT NULL AUTO_INCREMENT,
+                       title varchar(100),
+                       releaseYear int,
+                       director varchar(50),
+                       rating float,
+                       genre char(20),
+                       PRIMARY KEY (Id)
+); 
+DROP TABLE movie;
